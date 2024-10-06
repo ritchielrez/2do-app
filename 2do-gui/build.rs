faeb84c603efb821d03f3452f66e24710ab6070c
RitchielRez <ritchielrez@gmail.com>
2024-10-05 20:24:55 -0400

    chore(slint-build): use fluent-dark style

--- 2do-gui/build.rs
+++ 2do-gui/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    slint_build::compile("ui/hello.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new()
+        .with_style("fluent-dark".into());
+    slint_build::compile_with_config("ui/todo.slint", config).unwrap();
 }
