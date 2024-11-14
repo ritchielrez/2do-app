a879448746a9dd4cd4c66fd0cdc494ac7e767060
Ritchiel Reza <S300145332@ddsbstudent.ca>
2024-11-14 09:43:21 -0500

    fix(ui): use `native` styling

--- 2do-gui/build.rs
+++ 2do-gui/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
-        .with_style("fluent-dark".into());
+        .with_style("native".into());
     slint_build::compile_with_config("ui/todo.slint", config).unwrap();
 }
