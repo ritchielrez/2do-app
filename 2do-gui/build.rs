8fab66edd1cb1f9471b5cf5c481b0fdebfd802a3
RitchielRez <ritchielrez@gmail.com>
2024-12-14 18:41:42 -0500

    feat(2do-gui): clean start

--- 2do-gui/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let config = slint_build::CompilerConfiguration::new()
-        .with_style("native".into());
-    slint_build::compile_with_config("ui/todo.slint", config).unwrap();
-}
