ad7704b375ed87246aaba65e6665e774cb9a8fdb
ritchielrez <ritchielrez@gmail.com>
2025-02-28 23:07:25 -0500

    chore(ui): remove search button

--- 2do-gui/src/Search.tsx
+++ 2do-gui/src/Search.tsx
@@ -7,7 +7,6 @@ export default function Search(props: SearchProps) {
     return (
         <div id="search">
             <input value={props.value} onChange={(e) => props.setValue(e.target.value)} id="search-bar" type="textbox"></input>
-            <button id="search-btn">Search</button>
         </div>
     );
 }
