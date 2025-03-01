7c1cfbb2b1af32133e8b4b58bb8c7ae3fec2dbe2
ritchielrez <ritchielrez@gmail.com>
2025-02-28 20:44:56 -0500

    feat(ui): interactive todo list without search

--- 2do-gui/src/TodoList.tsx
+++ 2do-gui/src/TodoList.tsx
@@ -1,3 +1,33 @@
-export default function TodoList() {
+import { useState } from "react";
 
+type Todo = {
+    checked: boolean;
+    task: string;
+}
+
+export default function TodoList() {
+    let [todos, setTodos] = useState(Array<Todo>(
+        {checked: false, task: "task1"},
+        {checked: true, task: "task2"}
+    ));
+
+    const onTodoClick = (idx: number) => {
+        let todos_new = todos.slice();
+        todos_new[idx].checked = !todos_new[idx].checked;
+        setTodos(todos_new);
+    }
+
+    return(
+        <div id="todo-list">
+            {todos.map((todo, idx) => 
+                <label className="todo">
+                    {todo.checked ? 
+                        <input type="checkbox" onClick={() => onTodoClick(idx)} checked></input> 
+                        : <input type="checkbox" onClick={() => onTodoClick(idx)}></input>
+                    }
+                    {todo.task}
+                </label>
+            )}
+        </div>
+    )
 }
