15c6124557594bbd8d64ecbbf347ca7b861a570c
ritchielrez <ritchielrez@gmail.com>
2025-02-28 22:10:49 -0500

    refactor(ui): have all code related to search in one component

--- 2do-gui/src/Search.tsx
+++ 2do-gui/src/Search.tsx
@@ -1,20 +1,8 @@
-function SearchBar() {
-    return (
-        <input id="search-bar" type="textbox"></input>
-    );
-}
-
-function SearchBtn() {
-    return (
-        <button id="search-btn">Search</button>
-    );
-}
-
 export default function Search() {
     return (
         <div id="search">
-            <SearchBar></SearchBar>
-            <SearchBtn></SearchBtn>
+            <input id="search-bar" type="textbox"></input>
+            <button id="search-btn">Search</button>
         </div>
     );
 }
