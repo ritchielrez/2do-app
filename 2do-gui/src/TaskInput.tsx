f7047d851c186430c35860f7fff2b01a9a7e1dd4
ritchielrez <ritchielrez@gmail.com>
2025-04-24 15:50:15 -0400

    feat(ui): add `todosChange`

--- 2do-gui/src/TaskInput.tsx
+++ 2do-gui/src/TaskInput.tsx
@@ -4,17 +4,19 @@ import { useContext } from "react";
 export default function TaskInput() {
   const state = useContext(StateContext);
 
-  if (!state.newTask) return <></>;
+  if (state.newTask == undefined) return <></>;
   return (
     <>
       <input
         id="task-input"
         type="text"
+        value={state.newTask}
         onKeyDown={(event) => {
           if (event.key === "Escape") {
-            state.setNewTask(false);
+            state.setNewTask(undefined);
           }
         }}
+        onChange={(e) => {state.setNewTask(e.target.value)}}
       ></input>
     </>
   );
