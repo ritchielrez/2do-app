07c847ee1444c4dad3882f799cdd63ca5041c8dd
ritchielrez <ritchielrez@gmail.com>
2025-05-12 20:57:29 -0400

    feat(ui): add `parseData()` and `loadData()`

--- 2do-gui/src/Storage.ts
+++ 2do-gui/src/Storage.ts
@@ -1,1 +1,15 @@
-function uploadTodo() {}
+import { Todo } from "./App.tsx";
+
+function parseData(data: string) {
+}
+
+function loadData(): Array<Todo> {
+    const data = localStorage.getItem("todoList");
+    if (!data) {
+        localStorage.setItem("todoList", "- [ ] task1\n- [X] task2");
+        return Array<Todo>();
+    } else {
+        parseData(data);
+        return Array<Todo>();
+    }
+}
